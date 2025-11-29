3a26598153647d781ff68fec7b35b3d6513bdaaf
kjolly <jollykyllian@gmail.com>
2025-11-29 16:36:14 +0100

    start webserv init

--- srcs/webserv.cpp
+++ srcs/webserv.cpp
@@ -1,7 +1,14 @@
 #include <../include/webserv.hpp>
 
 WebServ::WebServ(std::string& conf) {
-    // 1.
+    std::vector<servConf> config_server;
+
+    confParser parser(conf);
+    config_server = parser.parse();
+    for (size_t i = 0; i < config_server.size(); i++) {
+        Server* new_server = new Server(config_server[i]);
+        this->servers.push_back(new_server);
+    }
 }
 
 WebServ::~WebServ() {}
